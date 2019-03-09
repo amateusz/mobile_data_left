f77e95e1ece6d130be994cd46f5bdbfd604d6890
Mateusz Grzywacz <amateusz.grzywacz@gmail.com>
2019-03-09 23:46:35 +0100

    rendering landing page

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
 
-@app.route('/', methods = ['GET', 'POST'])
-def main():
+@app.route('/', methods=['GET'])
+def entry():
     return render_template('dynamic.html')
+
+
+@app.route('/', methods=['POST'])
+def entry_form():
+    if request.form['username'] and request.form['password']:
+        # perform actual check
+        # maybe some loading screen
+        print("checking")
+        return entry()
+    else:
+        # return alert/error, that fields cannot be left empty.
+        flash('You were successfully logged in', 'error')
+        return redirect(url_for('entry'))
 
 
 if __name__ == '__main__':
