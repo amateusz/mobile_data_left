9df0339c896f47ed2d235fa26412174731f7c456
Mateusz Grzywacz <amateusz.grzywacz@gmail.com>
2019-03-09 23:20:16 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def main():
+    return render_template('dynamic.html')
+
+
+if __name__ == '__main__':
+    app.run()
