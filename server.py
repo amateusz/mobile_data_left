b8cebf16e0256344ac5e8e13ec7c58363817ab5d
Mateusz Grzywacz <amateusz.grzywacz@gmail.com>
2019-03-11 11:44:15 +0100

    header alignment: center

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, get_flashed_messages
 
-app = Flask(__name__)
-app.secret_key = 'some_secret'
+server = Flask(__name__)
+server.secret_key = 'some_secret'
 
 
-@app.route('/', methods=['GET'])
+@server.route('/', methods=['GET'])
 def entry():
     return render_template('welcome.html')
 
 
-@app.route('/', methods=['POST'])
+@server.route('/', methods=['POST'])
 def entry_form():
     if request.form['username'] and request.form['password']:
         # perform actual check
@@ -25,5 +25,14 @@ def entry_form():
         return redirect(url_for('entry'))
 
 
+@server.route('/services')
+def services_list():
+    accounts = [{'msdin': 123123123, 'operator': 'Dombo SA', 'GBdue': 12.5, 'dateDue': 13},
+                {'msdin': 123123123, 'operator': 'Dombo SA', 'GBdue': 12.5, 'dateDue': 13},
+                {'msdin': 123123123, 'operator': 'Dombo SA', 'GBdue': 12.5, 'dateDue': 13},
+                {'msdin': 123123123, 'operator': 'Dombo SA', 'GBdue': 12.5, 'dateDue': 13}]
+    return (render_template('services_list.html', accounts=accounts))
+
+
 if __name__ == '__main__':
-    app.run()
+    server.run(host='0.0.0.0', port=8000, debug=False)
